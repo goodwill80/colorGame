340fe2fe95a9eec469d84544101761c6ce3cff4c
kit jonathan ho <j.hokit80@gmail.com>
2016-10-19 00:58:26 +0800

    winning logic added

--- app.js
+++ app.js
@@ -14,15 +14,22 @@ var messageDisplay = document.getElementById("message");
 
 colorDisplay.textContent = pickedColor;
 
-for(var i = 0; i < colors.length; i++) {
+for ( var i = 0; i < colors.length; i++ ) {
   squares[i].style.background = colors[i];
   squares[i].addEventListener("click", function(){
     var clickedColor = this.style.background;
     if (clickedColor === pickedColor){
         messageDisplay.textContent = "That's correct!";
+        changeColors(clickedColor);
     } else {
       this.style.background = "#232324";
       messageDisplay.textContent = "Try again!";
     }
 });
+};
+
+function changeColors (color) {
+for ( var i = 0; i < squares.length; i++ ) {
+  squares[i].style.background = color;
+}
 };
