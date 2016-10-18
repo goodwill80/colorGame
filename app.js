c104954b202fa46803795afb647e720a320d9672
kit jonathan ho <j.hokit80@gmail.com>
2016-10-19 01:37:28 +0800

    added random function

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
 
@@ -33,3 +33,8 @@ for ( var i = 0; i < squares.length; i++ ) {
   squares[i].style.background = color;
 }
 };
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
